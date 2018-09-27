d28c4b86694278c74e8e6acf241d21c40de977a8
gzlj3 <gzlj3@163.com>
2018-09-27 21:50:50 +0800

    home

--- src/model/puzzlecards.js
+++ src/model/puzzlecards.js
@@ -1,3 +1,11 @@
+import request from '../util/request';  // request 是 demo 项目脚手架中提供的一个做 http 请求的方法，是对于 fetch 的封装，返回 Promise
+
+const delay = (millisecond) => {
+  return new Promise((resolve) => {
+    setTimeout(resolve, millisecond);
+  });
+};
+
 export default {
     namespace: 'puzzlecards',
     state: {
@@ -15,6 +23,21 @@ export default {
       ],
       counter: 100,
     },
+    effects: {
+      *queryInitCards(_, sagaEffects) {
+        const { call, put } = sagaEffects;
+        // const endPointURI = 'https://08ad1pao69.execute-api.us-east-1.amazonaws.com/dev/random_joke';
+        const endPointURI = '/dev/random_joke';
+  
+        const puzzle = yield call(request, endPointURI);
+        yield put({ type: 'addNewCard', payload: puzzle });
+  
+        yield call(delay, 3000);
+  
+        const puzzle2 = yield call(request, endPointURI);
+        yield put({ type: 'addNewCard', payload: puzzle2 });
+      }
+    },    
     reducers: {
       addNewCard(state, { payload: newCard }) {
         const nextCounter = state.counter + 1;
--- src/page/puzzlecards.js
+++ src/page/puzzlecards.js
@@ -18,25 +18,20 @@ const mapDispatchToProps = (dispatch) => {
       };
       dispatch(action);
     },
+    onDidMount: () => {
+      dispatch({
+        type: `${namespace}/queryInitCards`,
+      });
+    },    
   };
 };
 
 @connect(mapStateToProps,mapDispatchToProps)
 export default class PuzzleCardsPage extends Component {
-  // addNewCard = () => {
-  //   this.setState(prevState => {
-  //     const prevCardList = prevState.cardList;
-  //     this.counter += 1;
-  //     const card = {
-  //       id: this.counter,
-  //       setup: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit,',
-  //       punchline: 'sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-  //     };
-  //     return {
-  //       cardList: prevCardList.concat(card),
-  //     };
-  //   });
-  // }
+  componentDidMount() {
+    this.props.onDidMount();
+  }
+  
   render() {
     return (
       <div>
